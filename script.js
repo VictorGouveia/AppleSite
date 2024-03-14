44417a137e88124a7cc13004b810f5507263277b
Victor Gouveia <91400322+VictorGouveia@users.noreply.github.com>
2024-03-14 10:48:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+
+function changeCircle(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
